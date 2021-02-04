3009a43c71c2992843c9cc78b5294073d4b0e28e
Kanke <sugarkanke@yahoo.com>
2021-02-04 11:13:20 +0000

    Adding scss to project and configuring it in webpack

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/IndecisionApp";
+import './style/styles.scss';
 
 ReactDOM.render(<IndecisionApp options={['Devils den', 'Second district']}/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,28 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-        }]
+        }, /*run css with webpack
+            {
+                test: /\.css$/,
+                //allows us to specify an array of loaders
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },*/
+        //run scss with webpack
+        {
+             test: /\.scss$/,
+             //allows us to specify an array of loaders
+             use: [
+                'style-loader',
+                 'css-loader',
+                 'sass-loader'
+             ]
+        }],
+
     },
-    //sourcemaps
+    //sourcemaps - Helps you track errors In app
     devtool: 'cheap-module-eval-source-map',
 
     //lets webpack know where to serve public file. Alt to live-server
