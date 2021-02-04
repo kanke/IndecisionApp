b75128e86eacaaa67c090a12ed38a203cecd66d3
Kanke <sugarkanke@yahoo.com>
2021-02-04 16:50:14 +0000

    Adding widget style

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -19,7 +19,7 @@ export default class AddOption extends React.Component {
     render() {
         return (
             <div>
-                {this.state.error && <p>{this.state.error}</p>}
+                {this.state.error && <p className='widget__message'>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option"/>
                     <button className="button">Add Option</button>
